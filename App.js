dda79fdd88517cc7daea9e46357ae2e9ceb042f1
Ulibte <ulibte@outlook.com>
2020-12-04 00:39:32 -0300

    auth

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
 //import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet, StatusBar } from 'react-native';
 import MenuScreen from './screens/MenuScreen';
 import Dice from './screens/Dice';
 import Bright from './screens/Bright';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Login from './screens/loginScreen'
+import LoginScreen from './screens/loginScreen';
+import { isLoggedIn } from './api'
 
 export default function App() {
     StatusBar.setBarStyle('light-content');
     StatusBar.setBackgroundColor('black')
+
+    const [isLogged, setIsLogged] = useState(false)
+    isLoggedIn().then(res => setIsLogged(res))
+
     return (
         <View style={styles.container}>
-            <AppMenuNavigator />
+            <AppMenuNavigator isLogged={isLogged}/>
         </View>
     );
 }
 
 const AppStack = createStackNavigator()
-/* const BrightStack = createStackNavigator()
-const DiceStack = createStackNavigator()
-<BrightStack.Navigator>
-<BrightStack.Screen name="Bright" component={Bright}/>
-</BrightStack.Navigator>
-<DiceStack.Navigator>
-<DiceStack.Screen name="Dice" component={Dice}/>
-</DiceStack.Navigator> */
 
-const AppMenuNavigator = () => (
+const AppMenuNavigator = props => (
     <NavigationContainer>
-        <AppStack.Navigator screenOptions={{headerShown: false,}}>
+        {console.log(`prop logged = ${props.isLogged}`)}
+        {props.isLogged ? (<AppStack.Navigator screenOptions={{headerShown: false,}}>
             <AppStack.Screen name="Menu" component={MenuScreen}/>
             <AppStack.Screen name="Bright" component={Bright}/>
             <AppStack.Screen name="Dice" component={Dice}/>
+        </AppStack.Navigator>) : (
+            <AppStack.Navigator screenOptions={{headerShown: false,}}>
+            <AppStack.Screen name="Login" component={LoginScreen}/>
         </AppStack.Navigator>
+        )}
     </NavigationContainer>
 )
 
