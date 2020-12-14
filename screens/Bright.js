09c0567f45c6a0855b39530c961e9622d227a578
Ulibte <ulibte@outlook.com>
2020-12-13 21:53:34 -0300

    bright

--- screens/Bright.js
+++ screens/Bright.js
@@ -8,31 +8,26 @@ import Slider from '@react-native-community/slider';
 export default class Bright extends Component {
 
   state = {
-    bright: null,
-    brightBar: null,
+    brightSystem: 0,
+    brightSliderBar: null,
   }
 
   render() {
     return (
       <BackMenu pop={this.props.navigation.pop}>
         <View style={styles.vBrilho}>
-          <Text style={styles.text}>0 até 1</Text>
-          <Text style={styles.text}>Brilho: {this.state.bright}</Text>
-          <TextInput
-            keyboardType={'numeric'}
-            style={{ ...styles.text, ...styles.textInput }}
-            onChangeText={this.changeBrightAsync.bind(this)}
-            value={null}
-          />
+          <Text style={styles.text}>Brilho</Text>
+          <Text style={styles.text}>{this.transformNumberBright(this.state.brightSystem)}</Text>
           <Slider
-            style={{ width: '60%', height: 40 }}
+            style={{ width: '80%', height: 40 }}
             minimumValue={0.0001}
             maximumValue={1}
             minimumTrackTintColor="#FFFFFF"
             maximumTrackTintColor="#A72300"
             onValueChange={this.changeBrightAsync.bind(this)}
-            value={this.state.brightBar}
-            onSlidingComplete={null}
+            value={this.state.brightSliderBar}
+            onSlidingStart={this.onSlidingStartHandler}
+            onSlidingComplete={this.onSlidingCompleteHandler}
           />
         </View>
       </BackMenu>
@@ -49,35 +44,45 @@ export default class Bright extends Component {
     clearInterval(this.intervalBrightness)
   }
 
-  componentDidUpdate() {
+  /* componentDidUpdate() {
     console.log('componentDidUpdate ' + ++this.update);
-  }
+  } */
 
   async setBrightnessStateAsync() {
     const brightness = await Brightness.getSystemBrightnessAsync()
-    if (this.state.bright !== brightness) {
-      this.setState({ bright: brightness})
+    if (this.state.brightSystem !== brightness) {
+      this.setState({ brightSystem: brightness, brightSliderBar: brightness})
     }
   }
 
+  onSlidingStartHandler = () => {
+    clearInterval(this.intervalBrightness)
+  }
+
+  onSlidingCompleteHandler = () => {
+    this.intervalBrightness = setInterval(this.setBrightnessStateAsync.bind(this), 1000)
+  }
+
   async changeBrightAsync(value) {
-    this.setState({brightBar: value})
+    this.setState({brightSystem: value})
     const response = await Brightness.getPermissionsAsync()
     if (response.granted) {
-      await Brightness.setSystemBrightnessAsync(value)
+      Brightness.setSystemBrightnessAsync(value)
     } else {
       const { granted } = await Brightness.requestPermissionsAsync();
       if (granted) {
-        await Brightness.setSystemBrightnessAsync(value)
+        Brightness.setSystemBrightnessAsync(value)
       } else {
         alert('Sem permissão')
       }
     }
-    await this.setBrightnessStateAsync()
   }
 
-
-
+  transformNumberBright = number => {
+    number = number * 100
+    number = Math.floor(number)
+    return number + '%'
+  }
 
 }
 
@@ -94,7 +99,7 @@ const styles = StyleSheet.create({
   },
 
   text: {
-    fontSize: 40,
+    fontSize: 20,
     color: "white",
   },
 
